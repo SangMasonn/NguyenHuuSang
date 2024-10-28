437586ee175eb6df858df825510f1cc3bf253c78
NguyenHuuSang21 <154437789+NguyenHuuSang21@users.noreply.github.com>
2024-10-28 22:56:59 +0700

    sang

---
+++ src/bai1/Bai1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bai1;
+
+/**
+ *
+ * @author Admin
+ */
+public class Bai1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+ 
+        String hoTen = "Nguyen Huu Sang";
+        String lop = "CMU-TPM 15";
+        String maSoSinhVien = "123456789";
+
+  
+        System.out.println("Ten sinh vien: " + hoTen);
+        System.out.println("Lop: " + lop);
+        System.out.println("Ma so sinh vien: " + maSoSinhVien);
+    }
+}
+    
+
